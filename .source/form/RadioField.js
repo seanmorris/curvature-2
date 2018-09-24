d07027d879dc3878463ac19659edd9e6e2781e06
Sean Morris <sean@the-altar.virgo>
2018-09-24 19:20:20 -0400

    Radio buttons.

---
+++ .source/form/RadioField.js
@@ -0,0 +1,42 @@
+import { Field } from './Field';
+
+export class RadioField extends Field {
+	constructor(values, form, parent, key) {
+		super(values, form, parent, key);
+		this.template = `
+			<label
+				for       = "${this.args.name}"
+				data-type = "${this.args.attrs.type}"
+				cv-ref    = "label:curvature/base/Tag">
+				<span cv-if = "title">
+					<span cv-ref = "title:curvature/base/Tag">[[title]]</span>
+				</span>
+				<span
+					cv-each  = "options:option:optionText"
+					cv-carry = "value"
+					--cv-ref  = "input:curvature/base/Tag"
+				/>
+					<label>
+						<input
+							name    = "${this.args.name}"
+							type    = "radio"
+							value   = "[[option]]"
+							cv-bind = "value"
+					/>
+						[[optionText]]
+					</label>
+				</span>
+			</label>
+		`;
+	}
+	getLabel()
+	{
+		for(let i in this.args.options)
+		{
+			if(this.args.options[i] == this.args.value)
+			{
+				return i;
+			}
+		}
+	}
+}
