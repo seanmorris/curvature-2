bfe907efd772f829fde7d3d248575f7fbe405c05
Sean Morris <640101+seanmorris@users.noreply.github.com>
2023-03-03 00:38:13 -0500

    Ensuring map-coverage can run on node12.

--- test/map-coverage.js
+++ test/map-coverage.js
@@ -622,8 +622,15 @@ Promise.all(aggregateCoverage).then(() => {
 
 	const percentage = Number(100 * totalCovered / totalSize).toFixed(2);
 
+	const uuid = ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(
+		/[018]/g
+		, c => (
+			c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4
+		).toString(16)
+	);
+
 	fs.writeFileSync('./test/coverage/data/cv-coverage.json', JSON.stringify({
-		id: crypto.randomUUID()
+		id: uuid
 		, timestamp: Date.now()
 		// , hostname:  os.hostname()
 		// , user:      process.env.USER
